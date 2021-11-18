6456de012928d3cb2d771db57b97d1e89f0139a6
Steven Kedzie <seal.team.smyhk@gmail.com>
2021-11-18 10:44:48 -0800

    Define and register DbContext

---
+++ PlatformService/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlatformService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace PlatformService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
